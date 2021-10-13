c953627d82782b840443000f34820ba36390509f
EM <eric.mann2393@gmail.com>
2021-10-13 17:33:45 -0400

    remove angular fluff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SideSearchComponent } from './side-search/side-search.component';
+import { MainDisplayComponent } from './main-display/main-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideSearchComponent,
+    MainDisplayComponent
   ],
   imports: [
     BrowserModule
