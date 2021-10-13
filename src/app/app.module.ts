71ca0246b59e6133803f6ae2f39e2b079f50fe9b
EM <eric.mann2393@gmail.com>
2021-10-13 18:32:30 -0400

    restructure app component & routing display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SideSearchComponent } from './side-search/side-search.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideSearchComponent,
-    MainDisplayComponent
+    MainDisplayComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
