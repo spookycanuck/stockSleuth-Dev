f13bdc686bc59c7e0e74ce83801d86d280dad454
EM <eric.mann2393@gmail.com>
2021-10-14 14:25:04 -0400

    add footer & edit layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SideSearchComponent } from './side-search/side-search.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideSearchComponent,
     MainDisplayComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
