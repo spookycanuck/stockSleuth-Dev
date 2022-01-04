d79c0097ab9839ead8ba93c679315e1e84f24641
EM <eric.mann2393@gmail.com>
2022-01-03 21:03:39 -0500

    reorganized page components

---
+++ src/app/main-display/side-search/side-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-search',
+  templateUrl: './side-search.component.html',
+  styleUrls: []
+})
+export class SideSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
