56b3278a56bd8e59fe2ae158110a0aeed7289ad3
EM <eric.mann2393@gmail.com>
2022-01-03 21:04:05 -0500

    page routing stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { SideSearchComponent } from './side-search/side-search.component';
+import { SideSearchComponent } from './main-display/side-search/side-search.component';
 import { MainDisplayComponent } from './main-display/main-display.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ProfileComponent } from './text-components/profile/profile.component';
+import { AboutComponent } from './text-components/about/about.component';
+import { HelpComponent } from './text-components/help/help.component';
+import { TosComponent } from './text-components/tos/tos.component';
+import { PrivacyPolicyComponent } from './text-components/privacy-policy/privacy-policy.component';
+import { DisclaimerComponent } from './text-components/disclaimer/disclaimer.component';
 
 @NgModule({
   declarations: [
@@ -14,11 +21,18 @@ import { FooterComponent } from './footer/footer.component';
     SideSearchComponent,
     MainDisplayComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ProfileComponent,
+    AboutComponent,
+    HelpComponent,
+    TosComponent,
+    PrivacyPolicyComponent,
+    DisclaimerComponent
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
