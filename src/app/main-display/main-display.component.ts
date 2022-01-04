36a22a6e06465bd72b0500e128d2960b1aa25edc
EM <eric.mann2393@gmail.com>
2022-01-03 21:04:37 -0500

    refactor main/search features

--- src/app/main-display/main-display.component.ts
+++ src/app/main-display/main-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-main-display',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
