9f1f9909a2b7b3f3b101b7b98d84db23bd2b707a
EM <eric.mann2393@gmail.com>
2022-01-03 21:02:37 -0500

    add page routing/navigation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { MainDisplayComponent } from "./main-display/main-display.component";
+import { ProfileComponent } from "./text-components/profile/profile.component";
+import { AboutComponent } from "./text-components/about/about.component";
+import { HelpComponent } from "./text-components/help/help.component";
+import { TosComponent } from "./text-components/tos/tos.component";
+import { PrivacyPolicyComponent } from "./text-components/privacy-policy/privacy-policy.component";
+import { DisclaimerComponent } from "./text-components/disclaimer/disclaimer.component";
+
+const routes: Routes = [
+  { path: 'main', component: MainDisplayComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'signup', component: SignupComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'disclaimer', component: DisclaimerComponent },
+  { path: 'help', component: HelpComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'terms', component: TosComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  // providers: [AuthGuard] --> use later for route guarding
+})
+export class AppRoutingModule {}
