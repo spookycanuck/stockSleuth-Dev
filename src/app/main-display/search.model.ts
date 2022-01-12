a0ab6a679c171926566369da033164f1cde89c1f
EM <eric.mann2393@gmail.com>
2022-01-12 14:21:31 -0500

    intial commit of search model

---
+++ src/app/main-display/search.model.ts
@@ -0,0 +1,38 @@
+export interface Search {
+  id: number;
+  description: string;
+  ticker: string;
+  low: number;
+  high: number;
+}
+
+export const searches = [
+  {
+    id: 1,
+    description: 'Apple Inc.',
+    ticker: 'AAPL',
+    low: 236,
+    high: 252
+  },
+  {
+    id: 2,
+    description: 'Tesla Inc.',
+    ticker: 'TSLA',
+    low: 500,
+    high: 550
+  },
+  {
+    id: 3,
+    description: 'GameStop Corporation',
+    ticker: 'GME',
+    low: 150,
+    high: 169
+  },
+  {
+    id: 4,
+    description: 'Microsoft Corporation',
+    ticker: 'MSFT',
+    low: 290,
+    high: 320
+  },
+];
