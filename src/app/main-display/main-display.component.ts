44a6cc11bb6ef33af501ee817c38537cba284ca1
EM <eric.mann2393@gmail.com>
2022-01-12 14:25:34 -0500

    import search model to display user data

--- src/app/main-display/main-display.component.ts
+++ src/app/main-display/main-display.component.ts
@@ -1,16 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { searches } from './search.model';
 
 @Component({
   selector: 'app-main-display',
   templateUrl: './main-display.component.html',
-  styleUrls: ['./main-display.component.css']
+  styleUrls: ['./main-display.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainDisplayComponent implements OnInit {
+  searches = searches;
+  isLoading = false;
 
   constructor(private router: ActivatedRoute) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit() { }
 
 }
