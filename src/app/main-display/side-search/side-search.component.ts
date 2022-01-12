f3f8edb3580d9a857e12612f8e9e7a0bbce7ba90
EM <eric.mann2393@gmail.com>
2022-01-12 14:24:35 -0500

    removed side-search content. Not sure if needed anymore.

--- src/app/main-display/side-search/side-search.component.ts
+++ src/app/main-display/side-search/side-search.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideSearchComponent implements OnInit {
 
+  // I dont think I need this page anymore.
+
   constructor() { }
 
   ngOnInit(): void {
