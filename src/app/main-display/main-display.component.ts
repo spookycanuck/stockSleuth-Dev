0fcf7da19a65b25ccc1e70176b8b599209305617
EM <eric.mann2393@gmail.com>
2022-01-26 21:44:50 -0500

    add API call out to grab the JSON for a search. Notes included

--- src/app/main-display/main-display.component.ts
+++ src/app/main-display/main-display.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http'; //Angular likes HTTP client instead of async/fetch
+import { SearchService } from './search.service';
 
 import { searches } from './search.model';
 
@@ -12,9 +14,30 @@ import { searches } from './search.model';
 export class MainDisplayComponent implements OnInit {
   searches = searches;
   isLoading = false;
+  newSearch = '';
+  userValue = '';
+  returnStonkData = {};
 
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit() { }
 
+  onAddSearch(userInput: HTMLTextAreaElement) {
+    this.newSearch = userInput.value; //new variable name
+    console.log(this.newSearch);
+
+    const pythonURL = `http://localhost:5000/getPriceData?symbol=${this.newSearch}`
+    fetch(pythonURL).then(
+      res => res.json()
+    ).then(
+      data => {
+        this.returnStonkData = data
+        console.log(this.returnStonkData) //outputs API Data as the empty object returnStonkData
+      }
+    )
+
+    //Figure out a way to log returnStonkData outside of the scope of the API call.
+    //May need an observable - one of the bigger hurdles
+
+  }
 }
