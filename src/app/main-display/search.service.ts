26c0d300175405635eef6aef157f3fc19e46dedc
EM <eric.mann2393@gmail.com>
2022-01-26 21:45:04 -0500

    added notes on way forward

--- src/app/main-display/search.service.ts
+++ src/app/main-display/search.service.ts
@@ -4,4 +4,16 @@ import { Injectable } from "@angular/core";
 @Injectable({ providedIn: 'root' })
 export class SearchService {
 
+//Maybe two types of service?
+
+  // searchData
+  //   quick reference stuff:
+  //        daily high
+  //        daily low
+  //        company name
+
+
+  // priceData
+  //   graph stuff:
+  //       everything, including the above
 }
