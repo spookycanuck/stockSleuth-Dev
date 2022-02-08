b7ed0c336bbb98112ed5bd37afa30e7ac5de5079
EM <eric.mann2393@gmail.com>
2022-02-08 17:12:20 -0500

    commented out test data. Can remove soon.

--- src/app/main-display/search.model.ts
+++ src/app/main-display/search.model.ts
@@ -6,33 +6,33 @@ export interface Search {
   high: number;
 }
 
-export const searches = [
-  {
-    id: 1,
-    description: 'Apple Inc.',
-    ticker: 'AAPL',
-    low: 236,
-    high: 252
-  },
-  {
-    id: 2,
-    description: 'Tesla Inc.',
-    ticker: 'TSLA',
-    low: 500,
-    high: 550
-  },
-  {
-    id: 3,
-    description: 'GameStop Corporation',
-    ticker: 'GME',
-    low: 150,
-    high: 169
-  },
-  {
-    id: 4,
-    description: 'Microsoft Corporation',
-    ticker: 'MSFT',
-    low: 290,
-    high: 320
-  },
-];
+// export const searches = [
+//   {
+//     id: 1,
+//     description: 'Apple Inc.',
+//     ticker: 'AAPL',
+//     low: 236,
+//     high: 252
+//   },
+//   {
+//     id: 2,
+//     description: 'Tesla Inc.',
+//     ticker: 'TSLA',
+//     low: 500,
+//     high: 550
+//   },
+//   {
+//     id: 3,
+//     description: 'GameStop Corporation',
+//     ticker: 'GME',
+//     low: 150,
+//     high: 169
+//   },
+//   {
+//     id: 4,
+//     description: 'Microsoft Corporation',
+//     ticker: 'MSFT',
+//     low: 290,
+//     high: 320
+//   },
+// ];
