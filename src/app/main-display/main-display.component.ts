e7a8a9b1deb454f69a8d5dc8599bfc6cbd677556
EM <eric.mann2393@gmail.com>
2022-02-09 15:13:22 -0500

    add data binding from side-search to chart-display. completed task from yesterday.

--- src/app/main-display/main-display.component.ts
+++ src/app/main-display/main-display.component.ts
@@ -3,7 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http'; //Angular likes HTTP client instead of async/fetch
 import { SearchService } from './search.service';
 
-import { searches } from './search.model';
+import { Search } from './search.model';
 
 @Component({
   selector: 'app-main-display',
@@ -12,32 +12,16 @@ import { searches } from './search.model';
   encapsulation: ViewEncapsulation.None
 })
 export class MainDisplayComponent implements OnInit {
-  searches = searches;
-  isLoading = false;
-  newSearch = '';
-  userValue = '';
-  returnStonkData = {};
+  storedSearches = [];
 
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit() { }
 
-  onAddSearch(userInput: HTMLTextAreaElement) {
-    this.newSearch = userInput.value; //new variable name
-    console.log(this.newSearch);
-
-    const pythonURL = `http://localhost:5000/getPriceData?symbol=${this.newSearch}`
-    fetch(pythonURL).then(
-      res => res.json()
-    ).then(
-      data => {
-        this.returnStonkData = data
-        console.log(this.returnStonkData) //outputs API Data as the empty object returnStonkData
-      }
-    )
-
-    //Figure out a way to log returnStonkData outside of the scope of the API call.
-    //May need an observable - one of the bigger hurdles
-
+  onSearchAdded(search) {
+    this.storedSearches.push(search); //adds search to "searched" array
+    // console.log("main-display: ")
+    // console.log(this.storedSearches)
   }
+
 }
