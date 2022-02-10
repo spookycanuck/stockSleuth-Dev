e87dc628522cef2d159b49369c5f67b234c1b917
EM <eric.mann2393@gmail.com>
2022-02-10 16:05:39 -0500

    cleaned up code. continued search validation work.

--- src/app/main-display/side-search/side-search.component.ts
+++ src/app/main-display/side-search/side-search.component.ts
@@ -15,7 +15,8 @@ export class SideSearchComponent implements OnInit {
   isSubmitted = false;
   isLoading = false;
   newSearch = '';
-  userValue = '';
+  result = false;
+  stonkTicker = {};
   returnStonkData = {};
 
   // variable collection from Search feature
@@ -29,22 +30,58 @@ export class SideSearchComponent implements OnInit {
 
   ngOnInit() { }
 
+  stockList(userInput) {
+    const stockName = `http://localhost:5000/getStockList?symbol=${userInput}`
+    fetch(stockName).then(
+      res => res.json()
+    ).then(
+      data => {
+        this.stonkTicker = data
+        console.log(this.stonkTicker)
+        //outputs API Data as the empty object stonkTicker
+      }
+    )
+  }
+
+  searchAPI(userInput) {
+    const pythonURL = `http://localhost:5000/getPriceData?symbol=${userInput}`
+    fetch(pythonURL).then(
+      res => res.json()
+    ).then(
+      data => {
+        this.returnStonkData = data
+        console.log(this.returnStonkData)
+        //outputs API Data as the empty object returnStonkData
+      }
+    )
+  }
+
+  searchExists(userInput) {
+    var x = this.searches
+    var target = x.find(temp => temp.ticker == userInput)
+    if (target) {
+      // console.log("already searched!");
+      this.result = true;
+    }
+    else {
+      // console.log("doesn't exist")
+      this.result = false;
+    }
+    return(this.result)
+  }
+
   onAddSearch(form: NgForm) {
     this.isSubmitted = true;
     if (form.invalid) {
       return;
     }
     this.newSearch = form.value.search.toUpperCase(); //new variable name
-    // const pythonURL = `http://localhost:5000/getPriceData?symbol=${this.newSearch}`
-    // fetch(pythonURL).then(
-    //   res => res.json()
-    // ).then(
-    //   data => {
-    //     this.returnStonkData = data
-    //     console.log(this.returnStonkData)
-    //     //outputs API Data as the empty object returnStonkData
-    //   }
-    // )
+    this.searchExists(this.newSearch); //checking if ticker is already is in searches[]
+    if (this.result == true) {
+      return;
+    }
+    // this.stockList(this.newSearch); //checks if ticker is a valid ticker
+    // this.searchAPI(this.newSearch); //searches API for ticker data
 
     const search: Search = {
       //I want to pull the most recent API data into this model
@@ -59,13 +96,6 @@ export class SideSearchComponent implements OnInit {
     //   the array in the "Recent Searches" panel.
     this.searches.push(search);
     this.searchCreated.emit(search);
-    // console.log("side-search emit: ")
-    // console.log(this.searches)
-
-
-
-    //Figure out a way to log returnStonkData outside of the scope of the API call.
-    //May need an observable - one of the bigger hurdles
 
   }
 
