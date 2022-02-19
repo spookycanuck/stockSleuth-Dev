f9d9de91df2503293050dd9d04d8718eeb4e8ce4
EM <eric.mann2393@gmail.com>
2022-02-19 13:28:56 -0500

    remove unnecessary code

--- src/app/main-display/main-display.component.ts
+++ src/app/main-display/main-display.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http'; //Angular likes HTTP client instead of async/fetch
-import { SearchService } from './search.service';
 
 import { Search } from './search.model';
 
