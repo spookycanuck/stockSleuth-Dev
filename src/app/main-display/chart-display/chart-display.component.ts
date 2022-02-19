9005bb21f23463db7f385dddc0219a98b4b3aa2a
EM <eric.mann2393@gmail.com>
2022-02-19 13:29:35 -0500

    staged chart-display for creation of graphs. Incomplete.

--- src/app/main-display/chart-display/chart-display.component.ts
+++ src/app/main-display/chart-display/chart-display.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http'; //Angular likes HTTP client instead of async/fetch
-import { SearchService } from '../search.service';
+
 
 import { Search } from '../search.model';
 
@@ -14,8 +13,66 @@ export class ChartDisplayComponent implements OnInit {
   @Input() searches = [];
   isLoading = false;
 
+//   public graph = {
+//     data: [
+//         { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+//         { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+//     ],
+//     layout: {width: 320, height: 240, title: 'A Fancy Plot'}
+// };
+
+
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit() { }
+
+  chartGraph() {
+    var graph = {
+      data: {
+          x: this.searches[0].dates,
+          y: this.searches[0].adjCloses,
+          mode: 'lines',
+          marker: {
+              color: "black",
+              opacity: 1,
+          }
+      },
+
+      layout: {
+          title: `Adjusted Close`,
+          showlegend: true,
+          hovermode: 'x',
+          autosize: true,
+          legend: {"orientation": "h"},
+          margin: { l: 5, r: 5, t: 50, b: 5 },
+          xaxis: {
+              type: 'category',
+              categoryorder: "category ascending",
+              showspikes: true,
+              spikemode: 'across',
+              spikesnap: 'cursor',
+              showline: true,
+              showgrid: false,
+              showticklabels: true,
+              tickangle: 45,
+              autorange: true,
+              nticks: 12,
+              rangeslider: {visible: false},
+              automargin: true,
+          },
+          yaxis: {
+              showticklabels: false,
+              showgrid: false,
+              fixedrange: true,
+              automargin: true
+          }
+      },
+
+      config: {
+          responsive: true,
+          displayModeBar: false
+      }
+    }
+  }
 
 }
