c85dc5f72019025526aa28683a187959823f9b8b
EM <eric.mann2393@gmail.com>
2022-02-25 19:50:59 -0500

    add priceData type to Search model. Eliminates need for two subscribe methods. Allows me to handle the data in one object now, instead of two. Reduces redundant code.

--- src/app/main-display/search.model.ts
+++ src/app/main-display/search.model.ts
@@ -1,9 +1,12 @@
+import { PriceData } from "./priceData.model";
+
 export interface Search {
   id: number;
   description: string;
   ticker: string;
   low: number;
   high: number;
+  data: PriceData;
 }
 
 // export const searches = [
