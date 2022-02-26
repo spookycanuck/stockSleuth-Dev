1fc168702664e5569fe6f5040ce142323aaf18a4
EM <eric.mann2393@gmail.com>
2022-02-25 19:49:38 -0500

    pulled price data variable into its own model to reduce code in services

---
+++ src/app/main-display/priceData.model.ts
@@ -0,0 +1,9 @@
+export interface PriceData {
+  adjCloses: Array<number>;
+  colors: Array<string>;
+  dates: Array<Date>;
+  high: Array<number>;
+  low: Array<number>;
+  open: Array<number>;
+  volume: Array<number>;
+}
