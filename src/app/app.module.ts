268637f6cf5095f3cb9947e31035d6b551008d92
EM <eric.mann2393@gmail.com>
2022-03-15 20:47:44 -0400

    import angular materials tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTabsModule } from '@angular/material/tabs'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { MatPaginatorModule } from "@angular/material/paginator";
@@ -63,6 +64,7 @@ PlotlyModule.plotlyjs = PlotlyJS;
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
+    MatTabsModule,
     HttpClientModule,
     MatPaginatorModule,
     CommonModule,
