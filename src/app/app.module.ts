ec45e9e864617f4f4d5d8b87ce9d009240a09075
EM <eric.mann2393@gmail.com>
2022-04-05 17:47:28 -0400

    add reactive forms modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -55,6 +56,8 @@ PlotlyModule.plotlyjs = PlotlyJS;
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
     AppRoutingModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
