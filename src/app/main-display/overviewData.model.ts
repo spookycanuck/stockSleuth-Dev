69dd26395c1d7dfd7822b92d7c6049eece5b773e
EM <eric.mann2393@gmail.com>
2022-04-13 19:56:03 -0400

    initial commit of overview data model

---
+++ src/app/main-display/overviewData.model.ts
@@ -0,0 +1,18 @@
+export interface OverviewData {
+  name: string;
+  ceo: string;
+  industry: string;
+  sector: string;
+  cik: string;
+  isin: string;
+  cusip: string;
+  marketCap: number;
+  avgVol: number;
+  yrRange: string;
+  beta: number;
+  dcf: number;
+  ipoDate: Date;
+  actvTrading: string;
+  description: string;
+  website: string;
+}
