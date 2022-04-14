402aa5c26ed3f7b6773376bda6fd5afa93e69a57
EM <eric.mann2393@gmail.com>
2022-04-14 12:58:14 -0400

    add observable code for currentSearch, updates logically

--- src/app/main-display/chart-display/holders-tab/holders-tab.component.ts
+++ src/app/main-display/chart-display/holders-tab/holders-tab.component.ts
@@ -12,9 +12,12 @@ import { SearchService } from '../../search.service';
 export class HoldersTabComponent implements OnInit {
   searchList = []
   savedList = []
+  currentData = []
+  overviewData = []
 
   private searchesSub: Subscription;
   private savedSub: Subscription;
+  private currentSub: Subscription;
 
   constructor(public searchService: SearchService) {}
 
@@ -24,11 +27,39 @@ export class HoldersTabComponent implements OnInit {
     this.searchesSub = this.searchService.getSearchUpdateListener() //actively listening for new searches
     .subscribe((searches: Search[]) => {
       this.searchList = searches;
+      this.getCurrentData(this.searchList)
     });
     this.savedSub = this.searchService.getSavedUpdateListener() //actively listening for new searches
     .subscribe((saved: Search[]) => {
       this.savedList = saved;
+      this.getCurrentData(this.savedList)
     });
+    this.currentSub = this.searchService.getCurrentUpdateListener() //actively listening for new searches
+    .subscribe((data: Search[]) => {
+      this.currentData = data;
+      this.getCurrentData(this.currentData)
+    });
+    this.testList()
+  }
+
+  testList() {
+    var doody = JSON.parse(sessionStorage.getItem('currentSearch'))
+    this.overviewData = doody.overview.profile
+    // console.log(this.overviewData[0].value)
+  }
+
+  getCurrentData(data) {
+    // console.log(this.currentData)
+    if (data.length > 0) {
+      console.log(data[data.length-1])
+      var x = data[data.length-1];
+      this.overviewData = x.overview.profile
+    }
+    else if (data) {
+      var y = data;
+      this.overviewData = y.overview.profile
+    }
+    else return;
   }
 
 }
