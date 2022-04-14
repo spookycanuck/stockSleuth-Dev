622dbf3c16b9a4799bcd624e4717e19ae4945183
EM <eric.mann2393@gmail.com>
2022-04-14 12:52:36 -0400

    add observable for overview data w/ currentSearch object

--- src/app/main-display/chart-display/overview-tab/overview-tab.component.ts
+++ src/app/main-display/chart-display/overview-tab/overview-tab.component.ts
@@ -12,9 +12,13 @@ import { SearchService } from '../../search.service';
 export class OverviewTabComponent implements OnInit {
   searchList = []
   savedList = []
+  currentData = []
+  overviewData = []
+
 
   private searchesSub: Subscription;
   private savedSub: Subscription;
+  private currentSub: Subscription;
 
   constructor(public searchService: SearchService) {}
 
@@ -24,11 +28,38 @@ export class OverviewTabComponent implements OnInit {
     this.searchesSub = this.searchService.getSearchUpdateListener() //actively listening for new searches
     .subscribe((searches: Search[]) => {
       this.searchList = searches;
+      this.getCurrentData(this.searchList)
     });
     this.savedSub = this.searchService.getSavedUpdateListener() //actively listening for new searches
     .subscribe((saved: Search[]) => {
       this.savedList = saved;
+      this.getCurrentData(this.savedList)
     });
+    this.currentSub = this.searchService.getCurrentUpdateListener() //actively listening for new searches
+    .subscribe((data: Search[]) => {
+      this.currentData = data;
+      this.getCurrentData(this.currentData)
+    });
+    this.testList() // throws an error when commented out
   }
 
+  testList() {
+    var doody = JSON.parse(sessionStorage.getItem('currentSearch'))
+    this.overviewData = doody.overview.profile
+    // console.log(this.overviewData[0].value)
+  }
+
+  getCurrentData(data) {
+    console.log(this.currentData)
+    if (data.length > 0) {
+      // console.log(data[data.length-1])
+      var x = data[data.length-1];
+      this.overviewData = x.overview.profile
+    }
+    else if (data) {
+      var y = data;
+      this.overviewData = y.overview.profile
+    }
+    else return;
+  }
 }
