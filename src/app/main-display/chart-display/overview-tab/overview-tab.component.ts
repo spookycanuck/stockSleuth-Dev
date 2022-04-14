9f5b2cd575184b762ada9721c4a1fb8c2b130d90
EM <eric.mann2393@gmail.com>
2022-04-14 09:06:36 -0400

    add observable to update w/ searchList

--- src/app/main-display/chart-display/overview-tab/overview-tab.component.ts
+++ src/app/main-display/chart-display/overview-tab/overview-tab.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
+import { Search } from '../../search.model';
 import { SearchService } from '../../search.service';
 
 @Component({
@@ -11,11 +13,22 @@ export class OverviewTabComponent implements OnInit {
   searchList = []
   savedList = []
 
+  private searchesSub: Subscription;
+  private savedSub: Subscription;
+
   constructor(public searchService: SearchService) {}
 
   ngOnInit() {
     this.searchList = this.searchService.getSearches()
     this.savedList = this.searchService.getSaved()
+    this.searchesSub = this.searchService.getSearchUpdateListener() //actively listening for new searches
+    .subscribe((searches: Search[]) => {
+      this.searchList = searches;
+    });
+    this.savedSub = this.searchService.getSavedUpdateListener() //actively listening for new searches
+    .subscribe((saved: Search[]) => {
+      this.savedList = saved;
+    });
   }
 
 }
