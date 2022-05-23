3fa9fe1fbd948a5cda9a30b2d97581e71f7b749b
EM <eric.mann2393@gmail.com>
2022-05-23 17:44:51 -0400

    expand chart display variables for candlestick data

--- src/app/main-display/chart-display/graph.ts
+++ src/app/main-display/chart-display/graph.ts
@@ -1,71 +1,105 @@
-export const Graph = {
-    data: [
-      {
-        name: '',
-        x: [],
-        y: [],
-        mode: 'lines',
-        marker: {
-          color: "black",
-          opacity: 1,
-        }
-      },
-      {
-        marker: {
-            opacity: 0.5
-        },
-        type: 'bar',
-        yaxis: "y2"
-      },
-  ],
-
-    layout: {
-        title: '',
-        height: 900,
-        showlegend: true,
-        hovermode: 'x',
-        autosize: true,
-        legend: {"orientation": "v"},
-        margin: { l: 5, r: 5, t: 50, b: 5 },
-        xaxis: {
-            type: 'category',
-            categoryorder: "category ascending",
-            showspikes: true,
-            spikemode: 'across',
-            spikesnap: 'cursor',
-            showline: true,
-            showgrid: false,
-            showticklabels: true,
-            tickangle: 45,
-            autorange: true,
-            nticks: 12,
-            rangeslider: {visible: false},
-            automargin: true,
-        },
-        yaxis: {
-            showticklabels: true,
-            showgrid: true,
-            fixedrange: true,
-            automargin: true,
-            side: 'right'
-        },
-        yaxis2: {
-          visible: false,
-          showgrid: false,
-          title: 'Volume',
-          type: 'linear',
-          overlaying: 'y',
-          side: 'right',
-          marker: {
-              opacity: .5
-          },
-          fixedrange: true,
-          range: []
+export const Lines = {
+  data: [
+    {
+      name: '',
+      x: [],
+      y: [],
+      mode: 'lines',
+      marker: {
+        color: "black",
+        opacity: 1,
       }
     },
+    {
+      marker: {
+          opacity: 0.5
+      },
+      type: 'bar',
+      yaxis: "y2"
+    },
+  ],
 
-    config: {
-        responsive: true,
-        displayModeBar: false
-    }
+  config: {
+      responsive: true,
+      displayModeBar: false
   }
+}
+
+export const Candlestick = {
+  data: [
+    {
+      name: '',
+      x: [],
+      close: [],
+      decreasing: {
+        line: {color: 'red'}
+      },
+      increasing: {
+        line: {color: 'green'}
+      },
+      high: [],
+      low: [],
+      open: [],
+      type: 'candlestick',
+    },
+    {
+      marker: {
+          opacity: 0.5
+      },
+      type: 'bar',
+      yaxis: "y2"
+    },
+  ],
+
+  config: {
+      responsive: true,
+      displayModeBar: false
+  }
+}
+
+export const Layout = {
+  layout: {
+    title: '',
+    height: 900,
+    showlegend: true,
+    hovermode: 'x',
+    autosize: true,
+    legend: {"orientation": "v"},
+    margin: { l: 5, r: 5, t: 50, b: 5 },
+    xaxis: {
+        type: 'category',
+        categoryorder: "category ascending",
+        showspikes: true,
+        spikemode: 'across',
+        spikesnap: 'cursor',
+        showline: true,
+        showgrid: false,
+        showticklabels: true,
+        tickangle: 45,
+        autorange: true,
+        nticks: 12,
+        rangeslider: {visible: false},
+        automargin: true,
+    },
+    yaxis: {
+        showticklabels: true,
+        showgrid: true,
+        fixedrange: true,
+        automargin: true,
+        side: 'right'
+    },
+    yaxis2: {
+      visible: false,
+      showgrid: false,
+      title: 'Volume',
+      type: 'linear',
+      overlaying: 'y',
+      side: 'right',
+      marker: {
+          opacity: .5
+      },
+      fixedrange: true,
+      range: []
+  }
+}
+}
